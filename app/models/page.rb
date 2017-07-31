9359f92f3ce83b588a0c0cdb88df4a7c640989b9
Alexey <apasenko@gmail.com>
2017-07-31 13:41:31 +0000

    add test. time: 0:20

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,4 +1,9 @@
 class Page < ApplicationRecord
+  validates :name, presence: true
+  validates :name, length: { minimum: 4 }
+  validates :description, presence: true
+  validates :description, length: { minimum: 4 }
+  
   paginates_per 15
   
   scope :search, -> (name) { where('name LIKE :name or description LIKE :name or tags LIKE :name', name: '%' + name + '%') }
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe PagesController, type: :controller do
-
+  before do
+    15.times { |x| Page.create!( name: "Name #{x}", description: "Description #{x}", tags: "lemon orange" ) }
+  end
+  
+  describe 'GET #index' do
+    it "returns a successful 200 response" do
+      get :index
+      expect(response).to be_success
+    end
+    
+    it 'populates an array of all page if name not set' do
+      post :create, :params => { :name => '' }
+      expect(response.body).to_not be_nil
+    end
+    
+    it 'populates an array of all page if name set lemon' do
+      post :create, :params => { :name => 'lemon' }
+      expect(response.body).to_not be_nil
+    end
+    
+    it 'array is nil if name is lemonad' do
+      post :create, :params => { :name => 'lemonad' }
+      expect(response.body).to eq ""
+    end
+  end
 end
