0868e36abf2739430ff4dbc6611534e7720fed3a
Alexey <apasenko@gmail.com>
2017-07-31 13:29:49 +0000

    fix search. push in heroku. time: :45

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,11 @@
 class PagesController < ApplicationController
   before_action :find_page, only: [ :show, :edit ]
+  before_action :set_pages, only: [ :create, :index ]
   
   def create
-    @pages = Page.search(params[:search]).page(params[:page])
   end
   
   def index
-    @pages = Page.all.page(params[:page])
   end
   
   def new
@@ -17,5 +16,9 @@ class PagesController < ApplicationController
   
   def find_page
     @page = Page.find(params[:id])
+  end
+  
+  def set_pages
+    @pages = Page.search(params[:search].to_s).page(params[:page])
   end
 end
